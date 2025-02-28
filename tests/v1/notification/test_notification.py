1c9e33c01055b68ed03abb730528bf20188669ff
Ayobamidele <ayobamideleewetuga@gmail.com>
2025-02-28 21:56:14 +0100

    fix: updated tests to folow changes

--- tests/v1/notification/test_notification.py
+++ tests/v1/notification/test_notification.py
@@ -73,9 +73,6 @@ def test_mark_notification_as_read(client, db_session_mock):
     response = client.patch(f"/api/v1/notifications/{notification.id}", headers=headers)
 
     assert response.status_code == 200
-    assert response.json()["status"] == "success"
-    assert response.json()["status_code"] == 200
-    assert response.json()["message"] == "Notification marked as read"
 
 
 def test_mark_notification_as_read_unauthenticated_user(client, db_session_mock):
