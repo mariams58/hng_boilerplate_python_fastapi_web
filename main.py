02cd3f0aa24726999a544a6534159eee948803a7
iamprecieee <emmypresh777@gmail.com>
2025-02-28 12:49:35 +0100

    chore: remove restfox.dev from CORS allowed origins

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ origins = [
     "http://localhost:3000",
     "http://localhost:3001",
     'https://anchor-python.teams.hng.tech',
-    'https://restfox.dev',
 ]
 
 
@@ -119,7 +118,7 @@ async def validation_exception(request: Request, exc: RequestValidationError):
 
 
 @app.exception_handler(IntegrityError)
-async def integrity_exception(request: Request, exc: IntegrityError):
+async def exception(request: Request, exc: IntegrityError):
     """Integrity error exception handlers"""
 
     logger.exception(f"Exception occured; {exc}")
@@ -135,7 +134,7 @@ async def integrity_exception(request: Request, exc: IntegrityError):
 
 
 @app.exception_handler(Exception)
-async def global_exception(request: Request, exc: Exception):
+async def exception(request: Request, exc: Exception):
     """Other exception handlers"""
 
     logger.exception(f"Exception occured; {exc}")
