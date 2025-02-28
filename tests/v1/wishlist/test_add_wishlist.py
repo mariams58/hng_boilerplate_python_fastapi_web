1923c93a56927d8973c0be1a0c1d1719c70a0e5c
Afebu Balogun <balogunafebu22@gmail.com>
2025-02-28 19:26:51 +0100

    fix: fixed typo in test_add_to_wishlist_success, changed "waitlist" to "wishlist", effected changes on test to mock the custom exceptions created instead of HTTPException

--- tests/v1/wishlist/test_add_wishlist.py
+++ tests/v1/wishlist/test_add_wishlist.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 from main import app
 from api.v1.services.wishlist import wishlist_service
 from api.v1.services.user import user_service
-from fastapi import HTTPException
+from api.v1.services.wishlist import ProductNotFoundException, ProductAlreadyInWishlistException
 
 
 @pytest.fixture
@@ -33,12 +33,12 @@ class TestAddToWishlist:
 		response = client.post("/api/v1/wishlist/", json={"product_id": "test-product-id"})
 
 		assert response.status_code == 201
-		assert response.json()["message"] == "Product added to waitlist successfully"
+		assert response.json()["message"] == "Product added to wishlist successfully"
 
 	
 	@patch('api.v1.services.wishlist.wishlist_service.create')
 	def test_add_to_wishlist_product_not_found(self, mock_create, client):
-		mock_create.side_effect = HTTPException(status_code=404, detail="Product not found")
+		mock_create.side_effect = ProductNotFoundException("Product not found")
 		
 		response = client.post("/api/v1/wishlist/", json={"product_id": "invalid-product-id"})
 		
@@ -47,8 +47,10 @@ class TestAddToWishlist:
 
 	@patch('api.v1.services.wishlist.wishlist_service.create')
 	def test_add_to_wishlist_already_exists(self, mock_create, client):
-		mock_create.side_effect = HTTPException(status_code=400, detail="Product already in wishlist")
+		mock_create.side_effect = ProductAlreadyInWishlistException("Product already in wishlist")
 		
 		response = client.post("/api/v1/wishlist/", json={"product_id": "test-product-id"})
 		
 		assert response.status_code == 400
+
+	
