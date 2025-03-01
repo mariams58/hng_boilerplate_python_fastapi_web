2fb0e89a1e85a90184d383fb0683a033e3ac0508
DavidIfebueme <davidifebueme@gmail.com>
2025-03-01 01:01:18 +0100

    fix: added status code in response json as per new response standards

--- tests/v1/user/test_delete_user.py
+++ tests/v1/user/test_delete_user.py
@@ -65,6 +65,7 @@ def test_delete_user(mock_db_session, user_service_mock, mock_token_verification
     assert response.json() == {
         "message": "User successfully deleted",
         "status": "success",
+        "status_code": 200,
         "data": {},
     }
 
@@ -113,5 +114,6 @@ def test_delete_own_account(mock_db_session, user_service_mock, mock_own_account
     assert response.json() == {
         "message": "User successfully deleted",
         "status": "success",
+        "status_code": 200,
         "data": {},
     }
