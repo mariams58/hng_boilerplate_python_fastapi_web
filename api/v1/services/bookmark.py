1d3efa4df8d0deb9bcb7a13b827ee742ba84e6ab
mariams58 <mariams58@yahoo.com>
2025-03-02 09:10:07 +0100

    feat: Added feature branch for get all bookmark(s) endpoint

--- api/v1/services/bookmark.py
+++ api/v1/services/bookmark.py
@@ -2,8 +2,9 @@ from api.core.base.services import Service
 from api.v1.models.bookmark import Bookmark
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
+from typing import List
 
-class BookmarkService():
+class BookmarkService(Service):
     """Bookmark Job service functionality"""
     
     @staticmethod
@@ -33,5 +34,21 @@ class BookmarkService():
         db.commit()
         db.refresh(new_bookmark)
         return new_bookmark
+    
+
+
+    def fetch_all(self, db: Session, user_id: str) -> List[Bookmark]:
+        """
+        Fetches all bookmarks for the specified user.
+        
+        Args:
+            db (Session): SQLAlchemy database session.
+            user_id (str): The id of the user.
+            
+        Returns:
+            List[Bookmark]: A list of Bookmark objects.
+        """
+        bookmarks = db.query(Bookmark).filter(Bookmark.user_id == user_id).all()
+        return bookmarks
 
 bookmark_service = BookmarkService()
